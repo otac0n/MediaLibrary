07f8e859ee9fc8faeeb9a4c5ef234d1292a1e5f9
John Gietzen <john@gietzen.us>
2020-02-22 13:00:07 -0800

    Created the MediaLibrary project.

---
+++ MediaLibrary.Tests/ProgramTests.cs
@@ -0,0 +1,15 @@
+// Copyright © John Gietzen. All Rights Reserved. This source is subject to the MIT license. Please see license.md for more information.
+
+namespace MediaLibrary.Tests
+{
+    using Xunit;
+
+    public class ProgramTests
+    {
+        [Fact]
+        public void Who_When_What()
+        {
+            var subject = new Program();
+        }
+    }
+}
---
+++ MediaLibrary/Program.cs
@@ -0,0 +1,13 @@
+// Copyright © John Gietzen. All Rights Reserved. This source is subject to the MIT license. Please see license.md for more information.
+
+namespace MediaLibrary
+{
+    using System;
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
