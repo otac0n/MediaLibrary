d233a14796645bb8b23a7294df246a2c2b1ec1ce
Katie Gietzen <katie@gietzen.us>
2021-01-18 14:58:37 -0800

    Increased the font size in the tag rule editor and increased the default size.

--- MediaLibrary/EditTagRulesForm.Designer.cs
+++ MediaLibrary/EditTagRulesForm.Designer.cs
@@ -42,7 +42,7 @@ namespace MediaLibrary
             // 
             this.applyButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.applyButton.Enabled = false;
-            this.applyButton.Location = new System.Drawing.Point(221, 488);
+            this.applyButton.Location = new System.Drawing.Point(276, 515);
             this.applyButton.Name = "applyButton";
             this.applyButton.Size = new System.Drawing.Size(75, 23);
             this.applyButton.TabIndex = 1;
@@ -54,7 +54,7 @@ namespace MediaLibrary
             // 
             this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(140, 488);
+            this.cancelButton.Location = new System.Drawing.Point(195, 515);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(75, 23);
             this.cancelButton.TabIndex = 2;
@@ -66,7 +66,7 @@ namespace MediaLibrary
             // 
             this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.okButton.Enabled = false;
-            this.okButton.Location = new System.Drawing.Point(302, 488);
+            this.okButton.Location = new System.Drawing.Point(357, 515);
             this.okButton.Name = "okButton";
             this.okButton.Size = new System.Drawing.Size(75, 23);
             this.okButton.TabIndex = 3;
@@ -83,7 +83,7 @@ namespace MediaLibrary
             this.rulePages.Location = new System.Drawing.Point(12, 12);
             this.rulePages.Name = "rulePages";
             this.rulePages.SelectedIndex = 0;
-            this.rulePages.Size = new System.Drawing.Size(365, 470);
+            this.rulePages.Size = new System.Drawing.Size(420, 497);
             this.rulePages.TabIndex = 4;
             // 
             // defaultPage
@@ -92,7 +92,7 @@ namespace MediaLibrary
             this.defaultPage.Location = new System.Drawing.Point(4, 22);
             this.defaultPage.Name = "defaultPage";
             this.defaultPage.Padding = new System.Windows.Forms.Padding(3);
-            this.defaultPage.Size = new System.Drawing.Size(357, 444);
+            this.defaultPage.Size = new System.Drawing.Size(412, 471);
             this.defaultPage.TabIndex = 0;
             this.defaultPage.Text = "(Default)";
             this.defaultPage.UseVisualStyleBackColor = true;
@@ -105,12 +105,12 @@ namespace MediaLibrary
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.rules.Enabled = false;
-            this.rules.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rules.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rules.Location = new System.Drawing.Point(0, 0);
             this.rules.Multiline = true;
             this.rules.Name = "rules";
             this.rules.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.rules.Size = new System.Drawing.Size(357, 444);
+            this.rules.Size = new System.Drawing.Size(412, 471);
             this.rules.TabIndex = 1;
             this.rules.WordWrap = false;
             // 
@@ -120,7 +120,7 @@ namespace MediaLibrary
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.cancelButton;
-            this.ClientSize = new System.Drawing.Size(389, 523);
+            this.ClientSize = new System.Drawing.Size(444, 550);
             this.Controls.Add(this.rulePages);
             this.Controls.Add(this.okButton);
             this.Controls.Add(this.applyButton);
