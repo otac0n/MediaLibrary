a9d304a94ecaf1b14b6bca1d19132e1a968ffffd
John Gietzen <john@gietzen.us>
2021-05-08 23:47:17 -0700

    Force browsers to delete `img` and `video` tags to avoid delayed loading and strream corruption issues.

--- MediaLibrary.Web/src/app/display/display.component.ts
+++ MediaLibrary.Web/src/app/display/display.component.ts
@@ -1,13 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 import { SearchResult } from '../../schema';
 
 @Component({
-  selector: 'app-display',
-  templateUrl: './display.component.html',
-  styleUrls: ['./display.component.scss']
+    selector: 'app-display',
+    templateUrl: './display.component.html',
+    styleUrls: ['./display.component.scss']
 })
-export class DisplayComponent {
+export class DisplayComponent implements OnChanges {
     @Input()
     public searchResult: SearchResult;
+
+    public displayResult: SearchResult;
+
+    constructor(
+        private changeDetector: ChangeDetectorRef) {
+    }
+
+    public ngOnChanges(changes: SimpleChanges) {
+        const searchResult = changes.searchResult;
+        if (searchResult) {
+            this.displayResult = null;
+            if (searchResult.currentValue) {
+                setTimeout(() => {
+                    if (this.searchResult === searchResult.currentValue) {
+                        this.displayResult = this.searchResult;
+                        this.changeDetector.detectChanges();
+                    }
+                }, 0);
+            }
+        }
+    }
 }
